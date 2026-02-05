15666039b416a1998c046c53e4ecc159dd22100d
Vanessa <vanessa.dressler@hotmail.com>
2026-02-05 19:08:12 +0100

    Finales Refactoring: Magic Values und Funktionen extrahiert

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,5 +1,10 @@
 import json
 
+TEMPLATE_FILE = "animals_template.html"
+OUTPUT_FILE = "animals.html"
+DATA_FILE = "animals_data.json"
+PLACEHOLDER = "__REPLACE_ANIMALS_INFO__"
+
 
 def load_data(file_path):
     with open(file_path, "r", encoding="utf-8") as handle:
@@ -28,37 +33,44 @@ def serialize_animal(animal):
     output = '<li class="cards__item">\n'
 
     if name:
-        output += f'<div class="card__title">{name}</div>\n'
+        output += f'  <div class="card__title">{name}</div>\n'
 
-    output += '<p class="card__text">\n'
+    output += '  <p class="card__text">\n'
 
     if diet:
-        output += f'<strong>Diet:</strong> {diet}<br/>\n'
+        output += f'    <strong>Diet:</strong> {diet}<br/>\n'
     if first_location:
-        output += f'<strong>Location:</strong> {first_location}<br/>\n'
+        output += f'    <strong>Location:</strong> {first_location}<br/>\n'
     if animal_type:
-        output += f'<strong>Type:</strong> {animal_type}<br/>\n'
+        output += f'    <strong>Type:</strong> {animal_type}<br/>\n'
 
-    output += '</p>\n'
+    output += '  </p>\n'
     output += '</li>\n'
 
     return output
 
 
-def main():
-    animals_data = load_data("animals_data.json")
-
+def get_skins(animal_data_list):
     output = ""
-    for animal in animals_data:
+    for animal in animal_data_list:
         output += serialize_animal(animal)
+    return output
 
-    with open("animals_template.html", "r", encoding="utf-8") as handle:
+
+def write_html_to_file(html, file_path):
+    with open(file_path, "w", encoding="utf-8") as handle:
+        handle.write(html)
+
+
+def main():
+    animals_data = load_data(DATA_FILE)
+    output = get_skins(animals_data)
+
+    with open(TEMPLATE_FILE, "r", encoding="utf-8") as handle:
         template = handle.read()
 
-    new_html = template.replace("__REPLACE_ANIMALS_INFO__", output)
-
-    with open("animals.html", "w", encoding="utf-8") as handle:
-        handle.write(new_html)
+    new_html = template.replace(PLACEHOLDER, output)
+    write_html_to_file(new_html, OUTPUT_FILE)
 
 
 if __name__ == "__main__":
